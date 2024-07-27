c796c26bc3dbe26c0e8a0b2823471ad88e79bccd
Anoop Kumar <amt312002@gmail.com>
2024-07-28 00:39:30 +0530

    loadingAnimation

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -1,7 +1,9 @@
 import '../App.css';
+import img from '../img/boat.png';
 export default function Loading() {
     return (
         <div className="loadingbackground">
+            <img src={img}/>
             <div className="loader">
                 <div className="wave wave11"></div>
                 <div className="wave wave22"></div>
