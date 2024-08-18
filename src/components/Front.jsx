2ef326ab32d391e8e0b73fbcb39600b80141166c
Anoop Kumar <amt312002@gmail.com>
2024-08-18 13:47:40 +0530

    done

--- src/components/Front.jsx
+++ src/components/Front.jsx
@@ -1,9 +1,23 @@
 import '../App.css'
 import logo from '../img/logo2.png'
+import React from 'react'
+import { FaFileDownload } from "react-icons/fa"
+import { MdFileOpen } from "react-icons/md";
 export default function Front(){
+    const[visibleResume, setVisibleResume] = React.useState(false);
+  
+    const handleResumeClick=()=>{
+        setVisibleResume(!visibleResume);
+    }
     return(
         <div class="front" >
-            <section className='half' style={{transform:'translateY(-50px)'}} >
+            {visibleResume && 
+                <span id="resumePreview">
+                    <div onClick={handleResumeClick}>X</div>
+                    <iframe src="https://drive.google.com/file/d/1k56iPPt9Cvem7IzJsV-dtiZQ0BmnHtxE/preview"></iframe>
+                </span>
+            }
+            <section id="aboutmeinfo" className='half' style={{transform:'translateY(-50px)'}} >
                 <div style={{marginBottom:'3vh'}}>
                     <b style={{fontSize:'60px',}}>Anoop Kumar  </b> 
                 </div>
@@ -14,6 +28,18 @@ export default function Front(){
                 <b style={{fontSize:'20px'}}>Origin:</b> Chandigarh, India<br/><br/>
                 I have a passion for creating <strong>elegant</strong> and <strong>innovative</strong> websites, where I often develop <strong>custom logic</strong> for animations and functionality. My <strong>curiosity</strong> drives me to understand the intricacies behind each process I work on.<br/>This approach has consistently yielded <strong>successful results</strong>, particularly when collaborating with <strong>skilled</strong> colleagues.
                 </p>
+                <a>
+                    <button onClick={handleResumeClick}>
+                        View Resume
+                        <MdFileOpen/>
+                    </button>
+                </a>
+                <a href="https://drive.google.com/uc?export=download&id=1k56iPPt9Cvem7IzJsV-dtiZQ0BmnHtxE">
+                    <button>
+                        Download Resume
+                        <FaFileDownload/>
+                    </button>
+                </a>
             </section>
             <section className='logo half'><img src={logo} alt="" /></section>
         </div>
